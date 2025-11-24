a79a2041de9b0ce64013f43c666c7ddca72badeb
Amr Saleh <amrsalehma@gmail.com>
2025-11-25 01:03:59 +0200

    add custom scroll view to book detail view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -12,55 +12,63 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          children: [
-            const CustomBookDetailsAppBar(),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.438,
-              child: const CustomBookImageItem(),
-            ),
-            const SizedBox(height: 44),
-            Text(
-              'Subtle Art Of Not Giving A Fuck',
-              textAlign: TextAlign.center,
-              style: TextStyleManager.bold24.copyWith(
-                fontFamily: kGTSectraFine,
-              ),
-            ),
-            const SizedBox(height: 6),
-            Opacity(
-              opacity: 0.7,
-              child: Text(
-                'Mark Manson',
-                style: TextStyleManager.semiBold18.copyWith(
-                  fontWeight: FontWeight.w500,
-                  fontStyle: FontStyle.italic,
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const CustomBookDetailsAppBar(),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.438,
+                  child: const CustomBookImageItem(),
                 ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            const BookRating(),
-            const SizedBox(height: 38),
-            const BookAction(),
-            const SizedBox(height: 38),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'You can also check out',
-                style: TextStyleManager.regular14.copyWith(
-                  fontWeight: FontWeight.w600,
+                const SizedBox(height: 44),
+                Text(
+                  'Subtle Art Of Not Giving A Fuck',
+                  textAlign: TextAlign.center,
+                  style: TextStyleManager.bold24.copyWith(
+                    fontFamily: kGTSectraFine,
+                  ),
                 ),
-              ),
+                const SizedBox(height: 6),
+                Opacity(
+                  opacity: 0.7,
+                  child: Text(
+                    'Mark Manson',
+                    style: TextStyleManager.semiBold18.copyWith(
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const BookRating(),
+                const SizedBox(height: 46),
+                const BookAction(),
+                const SizedBox(height: 50),
+                Spacer(flex: 1,),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also check out',
+                    style: TextStyleManager.regular14.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                SimilarBooksListView(),
+                const SizedBox(height: 40),
+                Spacer(flex: 16,),
+              ],
             ),
-            const SizedBox(height: 16),
-            SimilarBooksListView(),
-            const SizedBox(height: 40),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
