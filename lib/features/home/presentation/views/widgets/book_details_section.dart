cdc6380f9c7f27f020a93481aa74d1efabec3122
Amr Saleh <amrsalehma@gmail.com>
2025-11-25 01:18:58 +0200

    refactor book details view

---
+++ lib/features/home/presentation/views/widgets/book_details_section.dart
@@ -0,0 +1,43 @@
+import 'package:book_store/constants.dart';
+import 'package:book_store/core/utils/text_style_manager.dart';
+import 'package:book_store/features/home/presentation/views/widgets/book_action.dart';
+import 'package:book_store/features/home/presentation/views/widgets/book_rating.dart';
+import 'package:book_store/features/home/presentation/views/widgets/custom_book_image_item.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.438,
+          child: const CustomBookImageItem(),
+        ),
+        const SizedBox(height: 44),
+        Text(
+          'Subtle Art Of Not Giving A Fuck',
+          textAlign: TextAlign.center,
+          style: TextStyleManager.bold24.copyWith(fontFamily: kGTSectraFine),
+        ),
+        const SizedBox(height: 6),
+        Opacity(
+          opacity: 0.7,
+          child: Text(
+            'Mark Manson',
+            style: TextStyleManager.semiBold18.copyWith(
+              fontWeight: FontWeight.w500,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+        const BookRating(),
+        const SizedBox(height: 38),
+        const BookAction(),
+      ],
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,6 @@
-import 'package:book_store/constants.dart';
-import 'package:book_store/core/utils/text_style_manager.dart';
-import 'package:book_store/features/home/presentation/views/widgets/book_action.dart';
-import 'package:book_store/features/home/presentation/views/widgets/book_rating.dart';
+import 'package:book_store/features/home/presentation/views/widgets/book_details_section.dart';
 import 'package:book_store/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
-import 'package:book_store/features/home/presentation/views/widgets/custom_book_image_item.dart';
-import 'package:book_store/features/home/presentation/views/widgets/similar_books_list_view.dart';
+import 'package:book_store/features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -16,54 +12,18 @@ class BookDetailsViewBody extends StatelessWidget {
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                const CustomBookDetailsAppBar(),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.438,
-                  child: const CustomBookImageItem(),
-                ),
-                const SizedBox(height: 44),
-                Text(
-                  'Subtle Art Of Not Giving A Fuck',
-                  textAlign: TextAlign.center,
-                  style: TextStyleManager.bold24.copyWith(
-                    fontFamily: kGTSectraFine,
-                  ),
-                ),
-                const SizedBox(height: 6),
-                Opacity(
-                  opacity: 0.7,
-                  child: Text(
-                    'Mark Manson',
-                    style: TextStyleManager.semiBold18.copyWith(
-                      fontWeight: FontWeight.w500,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                const BookRating(),
-                const SizedBox(height: 46),
-                const BookAction(),
-                const SizedBox(height: 50),
-                Spacer(flex: 1,),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'You can also check out',
-                    style: TextStyleManager.regular14.copyWith(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                SimilarBooksListView(),
-                const SizedBox(height: 40),
-                Spacer(flex: 16,),
+                CustomBookDetailsAppBar(),
+                BookDetailsSection(),
+                SizedBox(height: 50),
+                Spacer(flex: 1),
+                SimilarBooksSection(),
+                SizedBox(height: 40),
+                Spacer(flex: 16),
               ],
             ),
           ),
