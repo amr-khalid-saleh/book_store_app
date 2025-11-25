742dd27f9981a2789fa6b91036d8e20c2f46e772
Amr Saleh <amrsalehma@gmail.com>
2025-11-25 15:52:06 +0200

    add book model to home feature

---
+++ lib/features/home/data/models/book_model/book_model.dart
@@ -0,0 +1,245 @@
+class BookModel {
+  BookModel({
+      this.items,});
+
+  BookModel.fromJson(dynamic json) {
+    if (json['items'] != null) {
+      items = [];
+      json['items'].forEach((v) {
+        items?.add(Items.fromJson(v));
+      });
+    }
+  }
+  List<Items>? items;
+BookModel copyWith({  List<Items>? items,
+}) => BookModel(  items: items ?? this.items,
+);
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    if (items != null) {
+      map['items'] = items?.map((v) => v.toJson()).toList();
+    }
+    return map;
+  }
+
+}
+
+class Items {
+  Items({
+      this.kind, 
+      this.id, 
+      this.etag, 
+      this.selfLink, 
+      this.volumeInfo, 
+      this.saleInfo, 
+     });
+
+  Items.fromJson(dynamic json) {
+    kind = json['kind'];
+    id = json['id'];
+    etag = json['etag'];
+    selfLink = json['selfLink'];
+    volumeInfo = json['volumeInfo'] != null ? VolumeInfo.fromJson(json['volumeInfo']) : null;
+    saleInfo = json['saleInfo'] != null ? SaleInfo.fromJson(json['saleInfo']) : null;
+  }
+  String? kind;
+  String? id;
+  String? etag;
+  String? selfLink;
+  VolumeInfo? volumeInfo;
+  SaleInfo? saleInfo;
+Items copyWith({  String? kind,
+  String? id,
+  String? etag,
+  String? selfLink,
+  VolumeInfo? volumeInfo,
+  SaleInfo? saleInfo,
+}) => Items(  kind: kind ?? this.kind,
+  id: id ?? this.id,
+  etag: etag ?? this.etag,
+  selfLink: selfLink ?? this.selfLink,
+  volumeInfo: volumeInfo ?? this.volumeInfo,
+  saleInfo: saleInfo ?? this.saleInfo,
+);
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['kind'] = kind;
+    map['id'] = id;
+    map['etag'] = etag;
+    map['selfLink'] = selfLink;
+    if (volumeInfo != null) {
+      map['volumeInfo'] = volumeInfo?.toJson();
+    }
+    if (saleInfo != null) {
+      map['saleInfo'] = saleInfo?.toJson();
+    }
+    return map;
+  }
+
+}
+
+class SaleInfo {
+  SaleInfo({
+      this.country, 
+      this.saleability, 
+      this.isEbook,});
+
+  SaleInfo.fromJson(dynamic json) {
+    country = json['country'];
+    saleability = json['saleability'];
+    isEbook = json['isEbook'];
+  }
+  String? country;
+  String? saleability;
+  bool? isEbook;
+SaleInfo copyWith({  String? country,
+  String? saleability,
+  bool? isEbook,
+}) => SaleInfo(  country: country ?? this.country,
+  saleability: saleability ?? this.saleability,
+  isEbook: isEbook ?? this.isEbook,
+);
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['country'] = country;
+    map['saleability'] = saleability;
+    map['isEbook'] = isEbook;
+    return map;
+  }
+
+}
+
+class VolumeInfo {
+  VolumeInfo({
+      this.title, 
+      this.subtitle, 
+      this.authors, 
+      this.publisher, 
+      this.publishedDate, 
+      this.description, 
+      this.pageCount,
+      this.printType, 
+      this.categories, 
+      this.maturityRating, 
+      this.imageLinks,
+      this.language, 
+      this.previewLink, 
+      this.infoLink, 
+      this.canonicalVolumeLink,});
+
+  VolumeInfo.fromJson(dynamic json) {
+    title = json['title'];
+    subtitle = json['subtitle'];
+    authors = json['authors'] != null ? json['authors'].cast<String>() : [];
+    publisher = json['publisher'];
+    publishedDate = json['publishedDate'];
+    description = json['description'];
+    pageCount = json['pageCount'];
+    printType = json['printType'];
+    categories = json['categories'] != null ? json['categories'].cast<String>() : [];
+    maturityRating = json['maturityRating'];
+    imageLinks = json['imageLinks'] != null ? ImageLinks.fromJson(json['imageLinks']) : null;
+    language = json['language'];
+    previewLink = json['previewLink'];
+    infoLink = json['infoLink'];
+    canonicalVolumeLink = json['canonicalVolumeLink'];
+  }
+  String? title;
+  String? subtitle;
+  List<String>? authors;
+  String? publisher;
+  String? publishedDate;
+  String? description;
+  int? pageCount;
+  String? printType;
+  List<String>? categories;
+  String? maturityRating;
+  ImageLinks? imageLinks;
+  String? language;
+  String? previewLink;
+  String? infoLink;
+  String? canonicalVolumeLink;
+VolumeInfo copyWith({  String? title,
+  String? subtitle,
+  List<String>? authors,
+  String? publisher,
+  String? publishedDate,
+  String? description,
+  int? pageCount,
+  String? printType,
+  List<String>? categories,
+  String? maturityRating,
+  bool? allowAnonLogging,
+  String? contentVersion,
+  ImageLinks? imageLinks,
+  String? language,
+  String? previewLink,
+  String? infoLink,
+  String? canonicalVolumeLink,
+}) => VolumeInfo(  title: title ?? this.title,
+  subtitle: subtitle ?? this.subtitle,
+  authors: authors ?? this.authors,
+  publisher: publisher ?? this.publisher,
+  publishedDate: publishedDate ?? this.publishedDate,
+  description: description ?? this.description,
+  pageCount: pageCount ?? this.pageCount,
+  printType: printType ?? this.printType,
+  categories: categories ?? this.categories,
+  maturityRating: maturityRating ?? this.maturityRating,
+  imageLinks: imageLinks ?? this.imageLinks,
+  language: language ?? this.language,
+  previewLink: previewLink ?? this.previewLink,
+  infoLink: infoLink ?? this.infoLink,
+  canonicalVolumeLink: canonicalVolumeLink ?? this.canonicalVolumeLink,
+);
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['title'] = title;
+    map['subtitle'] = subtitle;
+    map['authors'] = authors;
+    map['publisher'] = publisher;
+    map['publishedDate'] = publishedDate;
+    map['description'] = description;
+    map['pageCount'] = pageCount;
+    map['printType'] = printType;
+    map['categories'] = categories;
+    map['maturityRating'] = maturityRating;
+    if (imageLinks != null) {
+      map['imageLinks'] = imageLinks?.toJson();
+    }
+    map['language'] = language;
+    map['previewLink'] = previewLink;
+    map['infoLink'] = infoLink;
+    map['canonicalVolumeLink'] = canonicalVolumeLink;
+    return map;
+  }
+
+}
+
+class ImageLinks {
+  ImageLinks({
+      this.smallThumbnail, 
+      this.thumbnail,});
+
+  ImageLinks.fromJson(dynamic json) {
+    smallThumbnail = json['smallThumbnail'];
+    thumbnail = json['thumbnail'];
+  }
+  String? smallThumbnail;
+  String? thumbnail;
+ImageLinks copyWith({  String? smallThumbnail,
+  String? thumbnail,
+}) => ImageLinks(  smallThumbnail: smallThumbnail ?? this.smallThumbnail,
+  thumbnail: thumbnail ?? this.thumbnail,
+);
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['smallThumbnail'] = smallThumbnail;
+    map['thumbnail'] = thumbnail;
+    return map;
+  }
+
+}
+
+
+
