c06e934214d312d0ed5afab989ee4ca5672843a7
Amr Saleh <amrsalehma@gmail.com>
2025-11-25 20:59:14 +0200

    implement fetch feature books

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -3,7 +3,7 @@ import 'package:book_store/features/home/data/models/book_model/book_model.dart'
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, BookModel>> fetchTrendingBooks();
+  Future<Either<Failure, List<BookModel>>> fetchTrendingBooks();
 
-  Future<Either<Failure, BookModel>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,18 +1,27 @@
 import 'package:book_store/core/error/failure.dart';
+import 'package:book_store/core/utils/api_services.dart';
 import 'package:book_store/features/home/data/models/book_model/book_model.dart';
 import 'package:book_store/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImpl implements HomeRepo{
+class HomeRepoImpl implements HomeRepo {
+  final ApiServices apiServices;
+
+  HomeRepoImpl({required this.apiServices});
+
   @override
-  Future<Either<Failure, BookModel>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      List<dynamic> books = await apiServices.get(url: '');
+      List<BookModel> booksList = books
+          .map((e) => BookModel.fromJson(e))
+          .toList();
+      return Right(booksList);
+    } catch (e) {
+      return Left(ServerFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, BookModel>> fetchTrendingBooks() {
-    // TODO: implement fetchTrendingBooks
-    throw UnimplementedError();
-  }
+  Future<Either<Failure, BookModel>> fetchTrendingBooks() {}
 }
