819560b8808bf71e2bc95835f1c9c2d9beabb942
Amr Saleh <amrsalehma@gmail.com>
2026-01-06 22:34:19 +0200

    fetch similar books & general books use cases

---
+++ lib/features/home/domain/use_cases/fetch_similar_books_use_case.dart
@@ -0,0 +1,18 @@
+import 'package:book_store/core/errors/failure.dart';
+import 'package:book_store/features/home/domain/entities/book_entity.dart';
+import 'package:book_store/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/use_cases/use_case.dart';
+
+class FetchSimilarBooksUseCase extends UseCase<List<BookEntity>,String> {
+  final HomeRepo homeRepo;
+
+  FetchSimilarBooksUseCase({required this.homeRepo});
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> execute(String category) async{
+    //check permission for user
+    return await homeRepo.fetchSimilarBooks(category: category);
+  }
+}
