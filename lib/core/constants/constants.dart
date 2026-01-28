3cd3adf715ed142a28984796c140c1e0d7828ff6
amr-khalid-saleh <amrsalehma@gmail.com>
2026-01-28 22:32:27 +0200

    register adapter and open general box

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -4,3 +4,4 @@ const kPrimaryColor = Color(0xff323232);
 const kTransitionDuration = Duration(milliseconds: 500);
 const kGTSectraFine = 'GT Sectra Fine';
 const kBaseUrl = 'https://www.googleapis.com/books/v1/';
+const kGeneralBooks = 'general_books';
