2a160f05e08cca9afc505bc4c15c349333547eb5
amr-khalid-saleh <amrsalehma@gmail.com>
2026-01-30 00:36:25 +0200

    refactor home repo implementation

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,4 +1,6 @@
 import 'package:book_store/core/utils/api_services.dart';
+import 'package:book_store/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:book_store/features/home/data/data_sources/home_remote_data_source.dart';
 import 'package:book_store/features/home/data/repos/home_repo_impl.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
@@ -8,6 +10,9 @@ final getIt = GetIt.instance;
 void setupServiceLocator() {
   getIt.registerSingleton<ApiServices>(ApiServices(Dio()));
   getIt.registerSingleton<HomeRepoImpl>(
-    HomeRepoImpl(apiServices: getIt.get<ApiServices>()),
+    HomeRepoImpl(
+      homeRemoteDataSource: getIt.get<HomeRemoteDataSource>(),
+      homeLocalDataSource: getIt.get<HomeLocalDataSource>(),
+    ),
   );
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,27 +1,39 @@
 import 'dart:developer';
 import 'package:book_store/core/errors/failure.dart';
-import 'package:book_store/core/utils/api_services.dart';
-import 'package:book_store/features/home/data/models/book_model/book_model.dart';
+import 'package:book_store/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:book_store/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:book_store/features/home/domain/entities/book_entity.dart';
 import 'package:book_store/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
-  final ApiServices apiServices;
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
 
-  HomeRepoImpl({required this.apiServices});
+  HomeRepoImpl({
+    required this.homeRemoteDataSource,
+    required this.homeLocalDataSource,
+  });
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchTrendingBooks() async {
+  Future<Either<Failure, List<BookEntity>>> fetchGeneralBooks() async {
     try {
-      var data = await apiServices.get(
-        endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest&q=general',
-      );
-      List<BookModel> booksList = [];
-      for (var item in data['items']) {
-        booksList.add(BookModel.fromJson(item));
+      var books = await homeRemoteDataSource.fetchGeneralBooks();
+      return Right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioException(e));
       }
-      return Right(booksList);
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchTrendingBooks() async {
+    try {
+      var books = await homeRemoteDataSource.fetchTrendingBooks();
+      return Right(books);
     } catch (e) {
       log(e.toString());
       if (e is DioException) {
@@ -32,38 +44,14 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchGeneralBooks() async {
-    try {
-      var data = await apiServices.get(
-        endPoint: 'volumes?Filtering=free-ebooks&q=general',
-      );
-      List<BookModel> booksList = [];
-      for (var item in data['items']) {
-        booksList.add(BookModel.fromJson(item));
-      }
-      return Right(booksList);
-    } catch (e) {
-      if (e is DioException) {
-        return Left(ServerFailure.fromDioException(e));
-      }
-      return Left(ServerFailure(e.toString()));
-    }
-  }
-
-  @override
-  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({
+  Future<Either<Failure, List<BookEntity>>> fetchSimilarBooks({
     required String category,
   }) async {
     try {
-      var data = await apiServices.get(
-        endPoint:
-            'volumes?Filtering=free-ebooks&Sorting=relevance&q=subject:$category',
+      var books = await homeRemoteDataSource.fetchSimilarBooks(
+        category: category,
       );
-      List<BookModel> booksList = [];
-      for (var item in data['items']) {
-        booksList.add(BookModel.fromJson(item));
-      }
-      return Right(booksList);
+      return Right(books);
     } catch (e) {
       if (e is DioException) {
         return Left(ServerFailure.fromDioException(e));
