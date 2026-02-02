a642c156ad8cfaaefb15f53e2156503f561e0a14
Amr Saleh <161505763+amr-khalid-saleh@users.noreply.github.com>
2026-02-02 17:17:14 +0200

    delete api kye

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -4,6 +4,6 @@ const kPrimaryColor = Color(0xff323232);
 const kTransitionDuration = Duration(milliseconds: 500);
 const kGTSectraFine = 'GT Sectra Fine';
 const kBaseUrl = 'https://www.googleapis.com/books/v1/';
-const kApiKey = 'AIzaSyDZSfOx3BNGATy34unC9RFxrupvVElwwIc';
 const kGeneralBooks = 'general_books';
 const kTrendingBooks = 'trending_books';
+
